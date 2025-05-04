f8e816e3c2ac46977f304fbe2ccf9f094bce9301
Kye <kyeelde@gmail.com>
2025-05-03 23:25:56 -0700

    Update index.ts

--- solutions/express/api/index.ts
+++ solutions/express/api/index.ts
@@ -29,7 +29,7 @@ app.get("/ask", async (req: Request, res: Response) => {
     res.setHeader("Content-Type", "text/plain");
 
 
-     res.write(response);
+     res.write(response.text);
 
     res.end();
   } 
